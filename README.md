0b7c53c6cedc4bc356be7beada5efb4e19199252
Chris Hobday <hobday.chris@protonmail.com>
2025-06-08 23:13:08 -0400

    Use Battlenet as a base and change naming to reflect, update registry file to use proper type, update script to use exe, change to extra-data/apply_extra

---
+++ README.md
@@ -0,0 +1,3 @@
+flatpak run org.flatpak.Builder build-dir --repo=repo --force-clean com.blizzard.Battlenet.HearthstoneDeckTracker.yml
+
+flatpak install --user ./repo com.blizzard.Battlenet.HearthstoneDeckTracker
--- Scripts/HearthstoneDeckTracker.sh
+++ Scripts/HearthstoneDeckTracker.sh
@@ -17,61 +17,21 @@ for i in {0..9}; do
     ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
-echo "Changing directory to /app/bin/HearthstoneDeckTracker"
-cd "/app/bin/HearthstoneDeckTracker"
+echo "Checking if Hearthstone Deck Tracker is already installed"
+if [ -d "${WINEPREFIX}/drive_c/users/chris/AppData/Local/Hearthstone Deck Tracker/" ]; then
+  echo "Hearthstone Deck Tracker is installed"
 
-echo "Launching Hearthstone Deck Tracker"
-wine "/app/bin/HearthstoneDeckTracker/Hearthstone Deck Tracker.exe"
+  echo "Changing directory to ${WINEPREFIX}/drive_c/users/chris/AppData/Local/Hearthstone Deck Tracker/"
+  cd "${WINEPREFIX}/drive_c/users/chris/AppData/Local/Hearthstone Deck Tracker/"
 
-echo "Checking if Battle.net is already installed"
-if [ -d "${WINEPREFIX}/drive_c/Program Files (x86)/Battle.net/" ]; then
-  echo "Battle.net is installed"
-
-  echo "Changing directory to ${WINEPREFIX}/drive_c/Program Files (x86)/Battle.net/"
-  cd "${WINEPREFIX}/drive_c/Program Files (x86)/Battle.net/"
-
-  echo "Launching Battle.net with Hearthstone flag"
-  wine "${WINEPREFIX}/drive_c/Program Files (x86)/Battle.net/Battle.net.exe" --exec="launch WTCG"
-
-  # TODO: Figure out why Hearthstone app remains running when Battle.net closes
+  echo "Launching Hearthstone Deck Tracker"
+  wine "${WINEPREFIX}/drive_c/users/chris/AppData/Local/Hearthstone Deck Tracker/Hearthstone Deck Tracker.exe"
 else
-  echo "Battle.net is not installed"
+  echo "Hearthstone Deck Tracker is not installed"
 
-  echo "Launching Battle.net Flatpak"
-  flatpak-spawn --host flatpak run com.blizzard.Battlenet
+  echo "Changing directory to /app/bin/"
+  cd "/app/bin/"
+
+  echo "Launching Hearthstone Deck Tracker Installer"
+  wine "/app/bin/HDT-Installer.exe"
 fi
-
-
-# #!/bin/sh
-# WINEPREFIX="$(echo ${HOME})/.var/app/com.blizzard.Battlenet/data/wine" # Required to reset the WINEPREFIX here because wine does not like the relative path set in the manifest
-
-# echo "Checking if Battle.net is already installed"
-# if [ -d "${WINEPREFIX}/drive_c/Program Files (x86)/Battle.net/" ]; then
-#   echo "Battle.net is installed"
-
-#   echo "Setting up Discord rich presence"
-#   for i in {0..9}; do
-#       test -S $XDG_RUNTIME_DIR/discord-ipc-$i ||
-#       ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
-#   done
-
-#   # echo "Installing dotnet472 with winetricks"
-#   # winetricks dotnet472
-
-#   # echo "Installing corefonts with winetricks"
-#   # winetricks corefonts
-
-#   # echo "Turning off crash dialogs with winetricks"
-#   # winetricks nocrashdialog
-
-#   # echo "Updating Windows registry with Wine graphics keys"
-#   # wine regedit /app/share/WineGraphics.reg
-
-
-#   # wine "${WINEPREFIX}/drive_c/Program Files (x86)/Battle.net/Battle.net.exe" --exec="launch WTCG"
-# else
-#   echo "Battle.net is not installed"
-
-#   echo "Launching Battle.net Flatpak"
-#   flatpak-spawn --host flatpak run com.blizzard.Battlenet
-# fi
