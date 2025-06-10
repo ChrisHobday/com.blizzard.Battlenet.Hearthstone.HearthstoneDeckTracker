d61c45272818c1b37400274009fcd3324de4e0d4
Chris Hobday <hobday.chris@protonmail.com>
2025-06-09 22:42:07 -0400

    Enable devel, switch to using HDT.zip instead of HDT.exe, and update script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 flatpak run org.flatpak.Builder build-dir --repo=repo --force-clean com.blizzard.Battlenet.HearthstoneDeckTracker.yml
 
 flatpak install --user ./repo com.blizzard.Battlenet.HearthstoneDeckTracker
+
+flatpak remove com.blizzard.Battlenet.HearthstoneDeckTracker
--- Scripts/HearthstoneDeckTracker.sh
+++ Scripts/HearthstoneDeckTracker.sh
@@ -8,6 +8,9 @@ echo "Updating registry"
 wine regedit /app/share/WineGraphics.reg
 sleep 3
 
+echo "Copying Hearthstone Deck Tracker installation directory into Wine prefix if it doesn't already exist"
+cp -nr "/app/extra/Hearthstone Deck Tracker" "${WINEPREFIX}/drive_c/Program Files (x86)/"
+
 echo "Installing dotnet48 with winetricks"
 winetricks dotnet48
 
@@ -17,21 +20,10 @@ for i in {0..9}; do
     ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
-echo "Checking if Hearthstone Deck Tracker is already installed"
-if [ -d "${WINEPREFIX}/drive_c/users/chris/AppData/Local/Hearthstone Deck Tracker/" ]; then
-  echo "Hearthstone Deck Tracker is installed"
+echo "Changing directory to /var/data/wine/drive_c/Program Files (x86)/Hearthstone Deck Tracker"
+cd "${WINEPREFIX}/drive_c/Program Files (x86)/Hearthstone Deck Tracker"
 
-  echo "Changing directory to ${WINEPREFIX}/drive_c/users/chris/AppData/Local/Hearthstone Deck Tracker/"
-  cd "${WINEPREFIX}/drive_c/users/chris/AppData/Local/Hearthstone Deck Tracker/"
+WINESYNC=1
 
-  echo "Launching Hearthstone Deck Tracker"
-  wine "${WINEPREFIX}/drive_c/users/chris/AppData/Local/Hearthstone Deck Tracker/Hearthstone Deck Tracker.exe"
-else
-  echo "Hearthstone Deck Tracker is not installed"
-
-  echo "Changing directory to /app/bin/"
-  cd "/app/bin/"
-
-  echo "Launching Hearthstone Deck Tracker Installer"
-  wine "/app/bin/HDT-Installer.exe"
-fi
+echo "Launching Hearthstone Deck Tracker"
+wine "${WINEPREFIX}/drive_c/Program Files (x86)/Hearthstone Deck Tracker/Hearthstone Deck Tracker.exe"
