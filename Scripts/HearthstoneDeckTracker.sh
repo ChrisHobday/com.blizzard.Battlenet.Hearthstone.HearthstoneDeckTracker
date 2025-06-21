8024a0944362fb8dca11d6857d7cafafcd569b4a
Chris Hobday <hobday.chris@protonmail.com>
2025-06-21 19:13:59 -0400

    Update org.wine.Sdk/Platform to 10.10, update script to not install dotnet48, and to run HDT directly from /app/extra

--- Scripts/HearthstoneDeckTracker.sh
+++ Scripts/HearthstoneDeckTracker.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 WINEPREFIX="$(echo ${HOME})/.var/app/com.blizzard.Battlenet/data/wine" # Required to reset the WINEPREFIX here because Wine does not like the relative path set in the manifest
 
-echo "Running Wineboot"
-wineboot
+# echo "Running Wineboot"
+# wineboot
 
 echo "Updating registry"
 wine regedit /app/share/WineGraphics.reg
 sleep 3
 
-echo "Copying Hearthstone Deck Tracker installation directory into Wine prefix if it doesn't already exist"
-cp -nr "/app/extra/Hearthstone Deck Tracker" "${WINEPREFIX}/drive_c/Program Files (x86)/"
+# echo "Copying Hearthstone Deck Tracker installation directory into Wine prefix if it doesn't already exist"
+# cp -nr "/app/extra/Hearthstone Deck Tracker" "${WINEPREFIX}/drive_c/Program Files (x86)/"
 
-echo "Installing dotnet48 with winetricks"
-winetricks dotnet48
+# echo "Installing dotnet48 with winetricks"
+# winetricks dotnet48
 
 echo "Setting up Discord rich presence"
 for i in {0..9}; do
@@ -20,10 +20,14 @@ for i in {0..9}; do
     ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
-echo "Changing directory to /var/data/wine/drive_c/Program Files (x86)/Hearthstone Deck Tracker"
-cd "${WINEPREFIX}/drive_c/Program Files (x86)/Hearthstone Deck Tracker"
+# echo "Changing directory to /var/data/wine/drive_c/Program Files (x86)/Hearthstone Deck Tracker"
+# cd "${WINEPREFIX}/drive_c/Program Files (x86)/Hearthstone Deck Tracker"
 
-WINESYNC=1
+# echo "Launching Hearthstone Deck Tracker"
+# wine "${WINEPREFIX}/drive_c/Program Files (x86)/Hearthstone Deck Tracker/Hearthstone Deck Tracker.exe"
+
+echo "Changing directory to /app/extra/Hearthstone Deck Tracker"
+cd "/app/extra/Hearthstone Deck Tracker"
 
 echo "Launching Hearthstone Deck Tracker"
-wine "${WINEPREFIX}/drive_c/Program Files (x86)/Hearthstone Deck Tracker/Hearthstone Deck Tracker.exe"
+wine "/app/extra/Hearthstone Deck Tracker/Hearthstone Deck Tracker.exe"
