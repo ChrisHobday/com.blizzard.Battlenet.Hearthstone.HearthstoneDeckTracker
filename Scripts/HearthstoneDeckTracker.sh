023a88b8bdc8fb862bf7a970397e43c86ff7f349
Chris Hobday <hobday.chris@protonmail.com>
2025-06-30 14:56:19 -0400

    Remove wineboot, move HDT directory to own user directory instead of Battle.net prefix

--- Scripts/HearthstoneDeckTracker.sh
+++ Scripts/HearthstoneDeckTracker.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 WINEPREFIX="$(echo ${HOME})/.var/app/com.blizzard.Battlenet/data/wine" # Required to reset the WINEPREFIX here because Wine does not like the relative path set in the manifest
 
-# echo "Running Wineboot"
-# wineboot
-
 echo "Updating registry"
 wine regedit /app/share/WineGraphics.reg
 sleep 3
 
-# echo "Copying Hearthstone Deck Tracker installation directory into Wine prefix if it doesn't already exist"
-# cp -nr "/app/extra/Hearthstone Deck Tracker" "${WINEPREFIX}/drive_c/Program Files (x86)/"
+# TODO: Update so that it is only copied if it is a newer version
+echo "Copying Hearthstone Deck Tracker installation directory to user data directory if it doesn't already exist"
+cp -nr "/app/extra/Hearthstone Deck Tracker" "/var/data/"
 
-# echo "Installing dotnet48 with winetricks"
-# winetricks dotnet48
+# dotnet48 is required to avoid crashes when starting/quitting Hearthstone
+echo "Installing dotnet48 with winetricks"
+winetricks dotnet48
 
 echo "Setting up Discord rich presence"
 for i in {0..9}; do
@@ -20,14 +19,8 @@ for i in {0..9}; do
     ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
-# echo "Changing directory to /var/data/wine/drive_c/Program Files (x86)/Hearthstone Deck Tracker"
-# cd "${WINEPREFIX}/drive_c/Program Files (x86)/Hearthstone Deck Tracker"
-
-# echo "Launching Hearthstone Deck Tracker"
-# wine "${WINEPREFIX}/drive_c/Program Files (x86)/Hearthstone Deck Tracker/Hearthstone Deck Tracker.exe"
-
 echo "Changing directory to /app/extra/Hearthstone Deck Tracker"
-cd "/app/extra/Hearthstone Deck Tracker"
+cd "/var/data/Hearthstone Deck Tracker"
 
 echo "Launching Hearthstone Deck Tracker"
-wine "/app/extra/Hearthstone Deck Tracker/Hearthstone Deck Tracker.exe"
+wine "/var/data/Hearthstone Deck Tracker/Hearthstone Deck Tracker.exe"
